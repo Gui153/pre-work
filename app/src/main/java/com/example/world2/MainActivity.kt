ebe890cf64d3aaa727756be1c42940b1011dd3e1
Guilherme Vilatoro Taglianeti <gv8@njit.edu>
2023-09-08 15:01:38 -0400

    Initial commit

---
+++ app/src/main/java/com/example/world2/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.world2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val but = findViewById<Button>(R.id.clickmeb)
+        but.setOnClickListener{
+            Log.v("Hi from Gui","Clicked!")
+            Toast.makeText(this, "Hello from Bob the cat", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
